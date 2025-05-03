1204fec6588e4781da0add5fd719c481bf9e1042
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-05-03 19:06:57 -0400

    sync

---
+++ answer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Multiplication, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Isaac Ip" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-indigo.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Multiplication, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Multiplication, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/multiplication.png" alt="Multiplication Table" width="250" />
+      </div>
+      <div class="page-content-php">
+        <div id="submit">
+          <?php
+          $lengthA = $_GET["value-1"];
+          $lengthB = $_GET["value-2"];
+          $product = 0;
+
+          // process 
+          let $product = 0;
+          for (let i = 0; i < value2; i++) {
+            product += value1;
+          }
+        document.getElementById("result").innerText += `${value1} x ${value2} = ${product}`;
+          ?>
+        </div>
+      </div>
+    </main>
+  </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,58 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="Multiplication, in PHP" />
+  <meta name="keywords" content="mths, icd2o" />
+  <meta name="author" content="Isaac Ip" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-indigo.min.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Multiplication, in PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">Multiplication, in PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div class="right-image">
+        <img src="./images/multiplication.png" alt="Multiplication Table" width="250" />
+      </div>
+      <br />
+      <div class="page-content">Please enter values below :
+      </div>
+      <div class="page-content-php">
+        <form action="answer.php" method="GET">
+          <br />
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input" type="text" id="value-1" name="value-1">
+            <label class="mdl-textfield__label" for="value-1">Value 1</label>
+          </div>
+          <br />
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input" type="text" id="value-2" name="value-2">
+            <label class="mdl-textfield__label" for="value-2">Value 2</label>
+          </div>
+          <br />
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent"
+            type="submit">
+            Calculate
+          </button>
+        </form>
+      </div>
+    </main>
+  </div>
+</body>
+
 </html>
